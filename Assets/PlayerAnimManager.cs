f6d883797430fa29ad7433098f53fb3259b13cd1
NoiseMak12 <M.ANISJKO-2023@hull.ac.uk>
2025-11-18 20:28:30 +0000

    added more sounds, test pls

--- Assets/PlayerAnimManager.cs
+++ Assets/PlayerAnimManager.cs
@@ -1,50 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Animations;
 
 public class PlayerAnimManager : MonoBehaviour
 {
     public Animator animator;
     public Rigidbody rb;
 
+    [Header("Audio")]
+    public AudioClip hitClip;
+    public AudioClip jumpClip;
+    public AudioClip walkClip;
+    public float soundVolume = 1f;
+    public float walkVolume = 0.5f;
+    public float walkFadeSpeed = 2f;
+
+    private AudioSource walkAudioSource;   // For looping walking sound
+    private AudioSource sfxAudioSource;    // For jump/hit one-shots
+    private float targetVolume = 0f;
+
     private void Start()
     {
-        //animator.SetBool("walk", true);
+        // Walking AudioSource
+        walkAudioSource = gameObject.AddComponent<AudioSource>();
+        walkAudioSource.clip = walkClip;
+        walkAudioSource.loop = true;
+        walkAudioSource.volume = 0f; // start muted
+        walkAudioSource.spatialBlend = 0f;
+        walkAudioSource.Play();
+
+        // SFX AudioSource for jump & hit
+        sfxAudioSource = gameObject.AddComponent<AudioSource>();
+        sfxAudioSource.loop = false;
+        sfxAudioSource.spatialBlend = 0f;
     }
+
     private void Update()
     {
         bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D);
 
-        if (isMoving)
-        {
-            animator.SetBool("w", true);
-        }
-        else
-        {
-            animator.SetBool("w", false);
-        }
-        //Debug.Log(isMoving);
+        // Walking animation & sound fade
+        animator.SetBool("w", isMoving);
+        targetVolume = isMoving ? walkVolume : 0f;
+        walkAudioSource.volume = Mathf.MoveTowards(walkAudioSource.volume, targetVolume, walkFadeSpeed * Time.deltaTime);
 
+        // Jump animation & sound
         if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.SetBool("j", true);
+            if (jumpClip != null)
+                sfxAudioSource.PlayOneShot(jumpClip, soundVolume);
         }
-        else
+        else if (Input.GetKeyUp(KeyCode.Space))
         {
             animator.SetBool("j", false);
-        }   
-        if (Input.GetMouseButton(0))
+        }
+
+        // Attack animation & sound
+        if (Input.GetMouseButtonDown(0))
         {
             animator.SetBool("s", true);
-            //animator.SetLayerWeight(1, 0.7f); // Sets layer 2
-
+            if (hitClip != null)
+                sfxAudioSource.PlayOneShot(hitClip, soundVolume);
         }
-        else
+        else if (Input.GetMouseButtonUp(0))
         {
             animator.SetBool("s", false);
-            //animator.SetLayerWeight(1, 0); // Sets layer 2 
         }
-
     }
 }
