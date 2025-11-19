7eef8b8da10e78af7809b16e92b84a62e6f9aab9
AdrianV2022 <146434555+DKchonkykong@users.noreply.github.com>
2025-11-19 14:27:17 +0000

    tried to fix overlapping text when death

--- Assets/nurseLines.cs
+++ Assets/nurseLines.cs
@@ -4,12 +4,13 @@ using UnityEngine;
 
 public class nurseLines : MonoBehaviour
 {
+    //added delay for speech so no more overlapping (hopefully)
     public Animator speak;
-
     public GameObject patchedUp;
-
-
     public GameObject talking;
+    public float dialogueDuration = 3f;
+    
+    private bool isTalking = false;
 
     public void Awake()
     {
@@ -19,14 +20,24 @@ public class nurseLines : MonoBehaviour
             patchedUp.SetActive(true);
         }
     }
+
     public void Yap()
     {
+        if (isTalking) return;
+
+        isTalking = true;
         clear();
         speak.StopPlayback();
         speak.Play("Speak");
         talking.SetActive(true);
-
+        Invoke("ResetTalking", dialogueDuration);
     }
+
+    private void ResetTalking()
+    {
+        isTalking = false;
+    }
+
     public void clear()
     {
         talking.SetActive(false);
